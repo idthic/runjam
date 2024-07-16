b83ce724eebda75449e9c0712a2fe40a527a3bff
Koichi Murase <myoga.murase@gmail.com>
2024-07-17 01:49:30 +0900

    util/context: sync with rfh

--- util/context.cpp
+++ util/context.cpp
@@ -1,81 +1,129 @@
-#include <cstdlib>
-#include <cstdio>
-#include <cstring>
 #include <cctype>
+#include <cinttypes>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
 #include "context.hpp"
 
 namespace idt {
 namespace util {
 
-const char* application_context::get_value(const char* key) const {
-  typedef dictionary_type::const_iterator It;
-  It it = m_data.find(key);
-  if (it != m_data.end())
-    return it->second.c_str();
-  return std::getenv(key);
-}
+  static int stricmp(const char* a, const char* b) {
+    while (*a && *b) {
+      int const diff = (int) std::tolower(*a++) - (int) std::tolower(*b++);
+      if (diff) return diff;
+    }
+    return *a ? 1 : *b ? -1 : 0;
+  }
 
-bool application_context::read_config(const char*& value, const char* key) const {
-  const char* str = get_value(key);
-  if (str == 0) return false;
-  value = str;
-  return true;
-}
-void application_context::set_config(const char* key, const char* value) {
-  this->set_value(key, value);
-}
+  const char* application_context::get_value(const char* name) const {
+    dictionary_type::const_iterator it = m_data.find(name);
+    if (it != m_data.end())
+      return it->second.c_str();
 
-bool application_context::read_config(std::string& value, const char* key) const {
-  const char* str = get_value(key);
-  if (str == 0) return false;
-  value = str;
-  return true;
-}
-void application_context::set_config(const char* key, std::string const& value) {
-  this->set_value(key, value);
-}
+    return std::getenv(name);
+  }
 
-bool application_context::read_config(int& value, const char* key) const {
-  const char* str= get_value(key);
-  if (str && std::strchr("+-0123456789", *str)) {
-    value = std::atoi(str);
-    return true;
-  } else
-    return false;
-}
-void application_context::set_config(const char* key, int value) {
-  char buff[100];
-  std::sprintf(buff, "%d", value);
-  m_data[key] = buff;
-}
+  bool application_context::assert_config(const char* name, const char* value) const {
+    return std::strcmp(this->get_config(name, value), value) == 0;
+  }
 
-bool application_context::read_config(double& value, const char* key) const {
-  const char* str= get_value(key);
-  if (str && std::strchr("+-.0123456789", *str)) {
-    value = std::atof(str);
-    return true;
-  } else
-    return false;
-}
-void application_context::set_config(const char* key, double value) {
-  char buff[100];
-  std::sprintf(buff, "%.15g", value);
-  m_data[key] = buff;
-}
+  bool application_context::read_config(const char*& var, const char* name) const {
+    const char* str = get_value(name);
+    if (str && str[0]) {
+      var = str;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  void application_context::set_config(const char* name, const char* value) {
+    this->set_value(name, value);
+  }
 
-bool application_context::read_config(bool& value, const char* key) const {
-  const char* str = get_value(key);
-  if (!str || !*str) return false;
+  bool application_context::read_config(std::string& var, const char* name) const {
+    const char* str = get_value(name);
+    if (str && str[0]) {
+      var = str;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  void application_context::set_config(const char* name, std::string const& value) {
+    this->set_value(name, value);
+  }
 
-  if (isdigit(*str))
-    value = std::atoi(str) != 0;
-  else
-    value = std::strcmp(str, "false") != 0;
-  return true;
-}
-void application_context::set_config(const char* key, bool value) {
-  set_config(key, value ? 1 : 0);
-}
+  bool application_context::read_config(int& var, const char* name) const {
+    const char* str = get_value(name);
+    if (str && std::strchr("+-0123456789", str[0])) {
+      var = std::atoi(str);
+      return true;
+    } else {
+      return false;
+    }
+  }
+  void application_context::set_config(const char* name, int value) {
+    char buff[64];
+    std::sprintf(buff, "%d", value);
+    this->set_value(name, buff);
+  }
+
+  bool application_context::read_config(std::uint32_t& var, const char* name) const {
+    const char* str = get_value(name);
+    if (str && str[0]) {
+      var = (std::uint32_t) strtoll(str, NULL, 10);
+      return true;
+    } else {
+      return false;
+    }
+  }
+  void application_context::set_config(const char* name, std::uint32_t value) {
+    char buff[32];
+    std::sprintf(buff, "%" PRIu32 "u", value);
+    this->set_value(name, buff);
+  }
+
+  bool application_context::read_config(double& var, const char* name) const {
+    const char* str = get_value(name);
+    if (str && std::strchr("+-.0123456789", str[0])) {
+      var = std::atof(str);
+      return true;
+    } else {
+      return false;
+    }
+  }
+  void application_context::set_config(const char* name, double value) {
+    char buff[32];
+    std::sprintf(buff, "%.15g", value);
+    this->set_value(name, buff);
+  }
+
+  bool application_context::read_config(bool& var, const char* name) const {
+    const char* str = get_value(name);
+    if (str && str[0]) {
+      if (stricmp(str, "true") == 0) {
+        var = true;
+        return true;
+      } else if (stricmp(str, "false") == 0) {
+        var = false;
+        return true;
+      } else {
+        char* end;
+        bool const value = std::strtoll(str, &end, 10);
+        if (*end) return false;
+        var = value;
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
+  void application_context::set_config(const char* name, bool value) {
+    this->set_value(name, value ? "true" : "false");
+  }
 
 }
 }
--- util/context.hpp
+++ util/context.hpp
@@ -1,53 +1,73 @@
+// -*- mode: c++; coding: utf-8 -*-
 #ifndef idt_util_context_hpp
 #define idt_util_context_hpp
-#include <map>
+#include <cstdint>
 #include <string>
+#include <utility>
+#if __cplusplus >= 201103L
+# include <unordered_map>
+#else
+# include <map>
+#endif
 
 namespace idt {
 namespace util {
 
   class application_context {
+#if __cplusplus >= 201103L
+    typedef std::unordered_map<std::string, std::string> dictionary_type;
+#else
     typedef std::map<std::string, std::string> dictionary_type;
+#endif
     dictionary_type m_data;
 
   public:
-    const char* get_value(const char* key) const;
-    void set_value(const char* key, const char* value) {
-      m_data[key] = value;
+    const char* get_value(const char* name) const;
+    void set_value(const char* name, const char* value) {
+      m_data[name] = value;
     }
-    void set_value(const char* key, std::string const& value) {
-      set_value(key, value.c_str());
+    void set_value(const char* name, std::string const& value) {
+      m_data[name] = value;
     }
+#if __cplusplus >= 201103L
+    void set_value(const char* name, std::string&& value) {
+      m_data[name] = std::move(value);
+    }
+#endif
 
   public:
-    void set_config(const char* key, const char* value);
-    void set_config(const char* key, std::string const& value);
-    void set_config(const char* key, int value);
-    void set_config(const char* key, double value);
-    void set_config(const char* key, bool value);
+    bool read_config(const char*& var, const char* name) const;
+    bool read_config(std::string& var, const char* name) const;
+    bool read_config(int& var, const char* name) const;
+    bool read_config(std::uint32_t& var, const char* name) const;
+    bool read_config(double& var, const char* name) const;
+    bool read_config(bool& var, const char* name) const;
 
   public:
-    bool read_config(const char*& value, const char* key) const;
-    bool read_config(std::string& value, const char* key) const;
-    bool read_config(int& value, const char* key) const;
-    bool read_config(double& value, const char* key) const;
-    bool read_config(bool& value, const char* key) const;
+    void set_config(const char* name, const char* value);
+    void set_config(const char* name, std::string const& value);
+    void set_config(const char* name, int value);
+    void set_config(const char* name, std::uint32_t value);
+    void set_config(const char* name, double value);
+    void set_config(const char* name, bool value);
 
   public:
     template<typename T>
-    bool read_config(T& value, const char* key, T const& defaultValue) const {
-      bool const ret = read_config(value, key);
-      if (!ret) value = defaultValue;
+    bool read_config(T& value, const char* name, T const& default_value) const {
+      bool const ret = this->read_config(value, name);
+      if (!ret) value = default_value;
       return ret;
     }
 
     template<typename T>
-    T get_config(const char* key, T const& defaultValue) const {
-      T value;
-      this->read_config<T>(value, key, defaultValue);
-      return value;
+    T get_config(const char* name, T default_value) const {
+      this->read_config(default_value, name);
+      return default_value;
     }
+
+    bool assert_config(const char* name, const char* value) const;
   };
+
 }
 }
 #endif
